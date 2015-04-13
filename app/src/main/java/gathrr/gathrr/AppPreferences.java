40f7cf59fdfa860ca0653a7a39fd80f9e43dcac0
Brian M <btm9786@rit.edu>
2015-04-13 13:51:27 -0400

    Added settings fields for Default Location
    Additionally, added some WIP code for making Settings Fields into
    clickable buttons

--- app/src/main/java/gathrr/gathrr/AppPreferences.java
+++ app/src/main/java/gathrr/gathrr/AppPreferences.java
@@ -2,14 +2,17 @@ package gathrr.gathrr;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by jshippling on 2/27/15.
  */
 
-public class AppPreferences extends PreferenceActivity {
+public class AppPreferences extends PreferenceActivity implements Preference.OnPreferenceClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +20,25 @@ public class AppPreferences extends PreferenceActivity {
 
         setDefaults();
         //this is deprecated but i don't really give a heck
+        /*
+        Preference button = (Preference)findPreference(getString(R.string.locationButton));
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                Toast.makeText(AppPreferences.this, "Set Location Coord", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        */
+
         addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference){
+        Log.i("Preferences", preference.getKey());
+        Toast.makeText(AppPreferences.this, "Set Location Coord", Toast.LENGTH_SHORT).show();
+        return true;
     }
 
     private void setDefaults(){
@@ -31,6 +52,8 @@ public class AppPreferences extends PreferenceActivity {
         p.putString("gender",gender);
         p.commit();
     }
+
+
 
 
 
