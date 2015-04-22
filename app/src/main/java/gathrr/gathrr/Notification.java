d844616e013f7a514265eadd4c9968dff7474978
Andrew <ajm5774@gmail.com>
2015-04-22 13:33:23 -0400

    notifications...not done

---
+++ app/src/main/java/gathrr/gathrr/Notification.java
@@ -0,0 +1,8 @@
+package gathrr.gathrr;
+
+/**
+ * Created by ajm57_000 on 4/13/2015.
+ */
+public class Notification {
+    public String Fighter, Date;
+}
---
+++ app/src/main/java/gathrr/gathrr/NotificationsActivity.java
@@ -0,0 +1,90 @@
+package gathrr.gathrr;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+import gathrr.utility.ApiHelper;
+
+
+public class NotificationsActivity extends ListActivity {
+
+    private String userId = "user4";
+    ListView listview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try {
+            setContentView(R.layout.activity_history);
+        }
+        catch(Exception ex){ ex.printStackTrace();}
+
+        listview = (ListView)findViewById(R.id.historylist);
+        CreateList task = new CreateList();
+        task.setActivity(this);
+        new Thread(task).start();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Log.d("menu", "preferences");
+                startActivity(new Intent(this, AppPreferences.class));
+                return true;
+            case R.id.action_history:
+                Log.d("menu","fight history");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private class CreateList implements Runnable {
+        public NotificationsActivity activity;
+        NotificationsAdapter notAdapter;
+
+        public void setActivity(NotificationsActivity a) {
+            this.activity = a;
+        }
+
+        @Override
+        public void run() {
+            JSONObject notifications = ApiHelper.getNotifications(userId);
+            notAdapter = new NotificationsAdapter(activity, notifications);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    listview.setAdapter(notAdapter);
+                }
+            });
+        }
+    }
+}
